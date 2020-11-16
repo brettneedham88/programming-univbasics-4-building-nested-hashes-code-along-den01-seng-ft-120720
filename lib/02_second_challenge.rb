bf24a6dda9cd08ab56f0be3cb5db6447e8d8b6a6
Brett Needham <brettneedham88@gmail.com>
2020-11-16 18:54:37 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,10 +1,14 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      
+      :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      
       }}
 
   
